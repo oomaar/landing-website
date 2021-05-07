27df0a2dfb027d3d29ee6404b197015d597ab14e
Omar <omaar_5@hotmail.com>
2021-05-08 01:14:34 +0200

    style: feature styles Responsive

--- src/components/Feature/styledFeature.js
+++ src/components/Feature/styledFeature.js
@@ -1,5 +1,30 @@
 import styled from "styled-components/macro";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding-top: 40px ;
+    background-color: #fff;
 
-export const SubContainer = styled.div``;
+    @media (max-width: 1190px) {
+		height: auto;
+    }
+
+    @media (max-width: 970px) {
+        margin-bottom: 100px;
+    }
+`;
+
+export const SubContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-content: center;
+
+    @media (max-width: 1190px) {
+		flex-wrap: wrap;
+    }
+`;
