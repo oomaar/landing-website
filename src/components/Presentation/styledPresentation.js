9f499befabeb7fca9c43a7e05e4465fa7bdf3f3d
Omar <omaar_5@hotmail.com>
2021-05-08 01:51:39 +0200

    style: presentation styles Responsive

--- src/components/Presentation/styledPresentation.js
+++ src/components/Presentation/styledPresentation.js
@@ -1,13 +1,20 @@
 import styled from "styled-components/macro";
 
 export const Container = styled.div`
-
+    padding-top: 30px;
+    width: 100%;
+    height: 768px;
+    position: relative;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-image: url("/images/presentation.png");
+    text-align: center;
 
     h1 {
-
+        font-size: 2rem;
     }
 
-    p {
-
+    @media (max-width: 970px) {
+        display: none;
     }
 `;
